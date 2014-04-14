55f501b152a675d38ec293375fcd613189ee43d6
svermeulen <sfvermeulen@gmail.com>
2014-04-14 00:27:26 -0300

    Bug fixes related to moving Zenject to its own repo

---
+++ Scripts/ZenjectMoqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using ModestTree.Zenject;
+
+#if UNITY_EDITOR && !UNITY_WEBPLAYER
+using Moq;
+#endif
+
+namespace ModestTree
+{
+    public static class ZenjectMoqExtensions
+    {
+        public static BindingConditionSetter AsMock<TContract>(this ReferenceBinder<TContract> binder) where TContract : class
+        {
+#if UNITY_EDITOR && !UNITY_WEBPLAYER
+            return binder.Bind(new SingletonInstanceProvider(Mock.Of<TContract>()));
+#else
+            Assert.That(false, "The use of 'AsMock' in web builds is not supported");
+            return null;
+#endif
+        }
+    }
+}
