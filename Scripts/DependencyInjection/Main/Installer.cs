a38826c824e28ccde23f5bb553b174e43758198c
svermeulen <sfvermeulen@gmail.com>
2014-05-03 15:21:21 -0300

    Resurrected multi-test scene installer and got a few old test beds working again.  General installer cleanup / refactoring.

--- Scripts/DependencyInjection/Main/Installer.cs
+++ Scripts/DependencyInjection/Main/Installer.cs
@@ -17,6 +17,13 @@ namespace ModestTree.Zenject
         }
 
         public abstract void RegisterBindings();
+
+        protected void Install<TInstaller>() where TInstaller : Installer, new()
+        {
+            var subInstaller = new TInstaller();
+            subInstaller.Container = _container;
+            subInstaller.RegisterBindings();
+        }
     }
 }
 
