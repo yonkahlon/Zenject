797721628968f8ffacbec65d5d4272b9d04c8083
svermeulen <sfvermeulen@gmail.com>
2014-06-05 14:39:36 -0300

    Added UndoableSet class

--- Scripts/Util/MiscExtensions.cs
+++ Scripts/Util/MiscExtensions.cs
@@ -16,5 +16,37 @@ namespace ModestTree
         {
             yield return item;
         }
+
+        // Most of the time when you call remove you always intend on removing something
+        // so assert in that case
+        public static void RemoveWithConfirm<T>(this IList<T> list, T item)
+        {
+            bool removed = list.Remove(item);
+            Assert.That(removed);
+        }
+
+        public static void RemoveWithConfirm<T>(this LinkedList<T> list, T item)
+        {
+            bool removed = list.Remove(item);
+            Assert.That(removed);
+        }
+
+        public static void RemoveWithConfirm<TKey, TVal>(this IDictionary<TKey, TVal> dictionary, TKey key)
+        {
+            bool removed = dictionary.Remove(key);
+            Assert.That(removed);
+        }
+
+        public static void RemoveWithConfirm<T>(this HashSet<T> set, T item)
+        {
+            bool removed = set.Remove(item);
+            Assert.That(removed);
+        }
+
+        public static void AddWithConfirm<T>(this HashSet<T> set, T item)
+        {
+            bool removed = set.Add(item);
+            Assert.That(removed);
+        }
     }
 }
