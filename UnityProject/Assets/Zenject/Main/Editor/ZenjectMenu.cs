1848b429ff49c8c6aa6da304019d6d42081135ec
svermeulen <sfvermeulen@gmail.com>
2014-10-13 11:04:04 -0300

    Added menu option to validate all scenes at once

--- UnityProject/Assets/Zenject/Main/Editor/ZenjectMenu.cs
+++ UnityProject/Assets/Zenject/Main/Editor/ZenjectMenu.cs
@@ -34,25 +34,61 @@ namespace ModestTree.Zenject
             AssetDatabase.Refresh();
         }
 
+        // Note that you can also use ZenEditorUtil.ValidateAllActiveScenes if you want the errors back
+        [MenuItem("Edit/Zenject/Validate All Active Scenes")]
+        public static bool ValidateAllActiveScenes()
+        {
+            var startScene = EditorApplication.currentScene;
+
+            var activeScenes = UnityEditor.EditorBuildSettings.scenes
+                .Select(x => new { Name = Path.GetFileNameWithoutExtension(x.path), Path = x.path }).ToList();
+
+            var failedScenes = new List<string>();
+
+            foreach (var sceneInfo in activeScenes)
+            {
+                Log.Info("Validating scene '{0}'...", sceneInfo.Name);
+
+                EditorApplication.OpenScene(sceneInfo.Path);
+
+                var compRoot = GameObject.FindObjectsOfType<CompositionRoot>().OnlyOrDefault();
+
+                // Do not validate if there is no comp root
+                if (compRoot != null)
+                {
+                    if (!ValidateCurrentScene())
+                    {
+                        Log.Error("Failed to validate scene '{0}'", sceneInfo.Name);
+                        failedScenes.Add(sceneInfo.Name);
+                    }
+                }
+            }
+
+            EditorApplication.OpenScene(startScene);
+
+            if (failedScenes.IsEmpty())
+            {
+                Log.Info("Successfully validated all {0} scenes", activeScenes.Count);
+                return true;
+            }
+            else
+            {
+                Log.Error("Validated {0}/{1} scenes. Failed to validate the following: {2}",
+                    activeScenes.Count-failedScenes.Count, activeScenes.Count, failedScenes.Join(", "));
+                return false;
+            }
+        }
+
         [MenuItem("Edit/Zenject/Validate Current Scene #%v")]
         public static bool ValidateCurrentScene()
         {
-            var compRoots = GameObject.FindObjectsOfType<CompositionRoot>();
+            var compRoot = GameObject.FindObjectsOfType<CompositionRoot>().OnlyOrDefault();
 
-            if (compRoots.HasMoreThan(1))
+            if (compRoot == null)
             {
-                Log.Error("Found multiple composition roots when only one was expected while validating current scene");
+                Log.Error("Unable to find unique composition root in current scene");
                 return false;
             }
-
-            if (compRoots.IsEmpty())
-            {
-                // Return true to allow playing in this case
-                Log.Error("Could not find composition root while validating current scene");
-                return true;
-            }
-
-            var compRoot = compRoots.Single();
 
             if (compRoot.Installers.IsEmpty())
             {
@@ -62,7 +98,7 @@ namespace ModestTree.Zenject
             }
 
             // Only show a few to avoid spamming the log too much
-            var resolveErrors = ValidateInstallers(compRoot).Take(10).ToList();
+            var resolveErrors = ZenEditorUtil.ValidateInstallers(compRoot).Take(10).ToList();
 
             foreach (var error in resolveErrors)
             {
@@ -77,51 +113,6 @@ namespace ModestTree.Zenject
 
             Log.Info("Validation Completed Successfully");
             return true;
-        }
-
-        static IEnumerable<ZenjectResolveException> ValidateInstallers(CompositionRoot compRoot)
-        {
-            var globalContainer = GlobalCompositionRoot.CreateContainer(true, null);
-            var container = compRoot.CreateContainer(true, globalContainer);
-
-            foreach (var error in container.ValidateResolve<IDependencyRoot>())
-            {
-                yield return error;
-            }
-
-            // Also make sure we can fill in all the dependencies in the built-in scene
-            foreach (var curTransform in compRoot.GetComponentsInChildren<Transform>())
-            {
-                foreach (var monoBehaviour in curTransform.GetComponents<MonoBehaviour>())
-                {
-                    if (monoBehaviour == null)
-                    {
-                        Log.Warn("Found null MonoBehaviour on " + curTransform.name);
-                        continue;
-                    }
-
-                    foreach (var error in container.ValidateObjectGraph(monoBehaviour.GetType()))
-                    {
-                        yield return error;
-                    }
-                }
-            }
-
-            foreach (var installer in globalContainer.InstalledInstallers.Concat(container.InstalledInstallers))
-            {
-                if (installer is IValidatable)
-                {
-                    foreach (var error in ((IValidatable)installer).Validate())
-                    {
-                        yield return error;
-                    }
-                }
-            }
-
-            foreach (var error in container.ValidateValidatables())
-            {
-                yield return error;
-            }
         }
 
         [MenuItem("Edit/Zenject/Output Object Graph For Current Scene")]
