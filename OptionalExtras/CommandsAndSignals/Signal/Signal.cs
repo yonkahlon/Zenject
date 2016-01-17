d88a2453d26f61c4f6a918806d39628b9093f599
Steve Vermeulen <sfvermeulen@gmail.com>
2016-01-17 00:39:58 -0400

    Minor change, made signal and trigger abstract and added ability to get signal from trigger

--- OptionalExtras/CommandsAndSignals/Signal/Signal.cs
+++ OptionalExtras/CommandsAndSignals/Signal/Signal.cs
@@ -8,14 +8,22 @@ namespace Zenject.Commands
     }
 
     // Zero Parameters
-    public class Signal : ISignal
+    public abstract class Signal : ISignal
     {
         public event Action Event = delegate {};
 
-        public class TriggerBase
+        public abstract class TriggerBase
         {
             [Inject]
             Signal _signal = null;
+
+            public Signal Signal
+            {
+                get
+                {
+                    return _signal;
+                }
+            }
 
             public void Fire()
             {
@@ -25,14 +33,22 @@ namespace Zenject.Commands
     }
 
     // One Parameter
-    public class Signal<TParam1> : ISignal
+    public abstract class Signal<TParam1> : ISignal
     {
         public event Action<TParam1> Event = delegate {};
 
-        public class TriggerBase
+        public abstract class TriggerBase
         {
             [Inject]
             Signal<TParam1> _signal = null;
+
+            public Signal<TParam1> Signal
+            {
+                get
+                {
+                    return _signal;
+                }
+            }
 
             public void Fire(TParam1 arg1)
             {
@@ -42,14 +58,22 @@ namespace Zenject.Commands
     }
 
     // Two Parameters
-    public class Signal<TParam1, TParam2> : ISignal
+    public abstract class Signal<TParam1, TParam2> : ISignal
     {
         public event Action<TParam1, TParam2> Event = delegate {};
 
-        public class TriggerBase
+        public abstract class TriggerBase
         {
             [Inject]
             Signal<TParam1, TParam2> _signal = null;
+
+            public Signal<TParam1, TParam2> Signal
+            {
+                get
+                {
+                    return _signal;
+                }
+            }
 
             public void Fire(TParam1 arg1, TParam2 arg2)
             {
@@ -59,14 +83,22 @@ namespace Zenject.Commands
     }
 
     // Three Parameters
-    public class Signal<TParam1, TParam2, TParam3> : ISignal
+    public abstract class Signal<TParam1, TParam2, TParam3> : ISignal
     {
         public event Action<TParam1, TParam2, TParam3> Event = delegate {};
 
-        public class TriggerBase
+        public abstract class TriggerBase
         {
             [Inject]
             Signal<TParam1, TParam2, TParam3> _signal = null;
+
+            public Signal<TParam1, TParam2, TParam3> Signal
+            {
+                get
+                {
+                    return _signal;
+                }
+            }
 
             public void Fire(TParam1 arg1, TParam2 arg2, TParam3 arg3)
             {
@@ -76,14 +108,22 @@ namespace Zenject.Commands
     }
 
     // Four Parameters
-    public class Signal<TParam1, TParam2, TParam3, TParam4> : ISignal
+    public abstract class Signal<TParam1, TParam2, TParam3, TParam4> : ISignal
     {
         public event Action<TParam1, TParam2, TParam3, TParam4> Event = delegate {};
 
-        public class TriggerBase
+        public abstract class TriggerBase
         {
             [Inject]
             Signal<TParam1, TParam2, TParam3, TParam4> _signal = null;
+
+            public Signal<TParam1, TParam2, TParam3, TParam4> Signal
+            {
+                get
+                {
+                    return _signal;
+                }
+            }
 
             public void Fire(TParam1 arg1, TParam2 arg2, TParam3 arg3, TParam4 arg4)
             {
@@ -93,14 +133,22 @@ namespace Zenject.Commands
     }
 
     // Five Parameters
-    public class Signal<TParam1, TParam2, TParam3, TParam4, TParam5> : ISignal
+    public abstract class Signal<TParam1, TParam2, TParam3, TParam4, TParam5> : ISignal
     {
         public event ModestTree.Util.Action<TParam1, TParam2, TParam3, TParam4, TParam5> Event = delegate {};
 
-        public class TriggerBase
+        public abstract class TriggerBase
         {
             [Inject]
             Signal<TParam1, TParam2, TParam3, TParam4, TParam5> _signal = null;
+
+            public Signal<TParam1, TParam2, TParam3, TParam4, TParam5> Signal
+            {
+                get
+                {
+                    return _signal;
+                }
+            }
 
             public void Fire(TParam1 arg1, TParam2 arg2, TParam3 arg3, TParam4 arg4, TParam5 arg5)
             {
@@ -110,14 +158,22 @@ namespace Zenject.Commands
     }
 
     // Six Parameters
-    public class Signal<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6> : ISignal
+    public abstract class Signal<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6> : ISignal
     {
         public event ModestTree.Util.Action<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6> Event = delegate {};
 
-        public class TriggerBase
+        public abstract class TriggerBase
         {
             [Inject]
             Signal<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6> _signal = null;
+
+            public Signal<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6> Signal
+            {
+                get
+                {
+                    return _signal;
+                }
+            }
 
             public void Fire(TParam1 arg1, TParam2 arg2, TParam3 arg3, TParam4 arg4, TParam5 arg5, TParam6 arg6)
             {
