720279d1d9fb9e5beafef562900919e488f1fedf
Steve Vermeulen <sfvermeulen@gmail.com>
2016-06-07 20:36:24 -0300

    Removed try-catch blocks around IInitializable.Initialize and IDisposable.Dispose to make debugging easier. re issue #123

--- UnityProject/Assets/Zenject/Source/Runtime/DisposableManager.cs
+++ UnityProject/Assets/Zenject/Source/Runtime/DisposableManager.cs
@@ -64,15 +64,7 @@ namespace Zenject
             {
                 Log.Debug("Disposing '" + disposable.Disposable.GetType() + "'");
 
-                try
-                {
-                    disposable.Disposable.Dispose();
-                }
-                catch (Exception e)
-                {
-                    throw Assert.CreateException(
-                        e, "Error occurred while disposing IDisposable with type '{0}'", disposable.Disposable.GetType().Name());
-                }
+                disposable.Disposable.Dispose();
             }
 
             Log.Debug("Disposed of {0} disposables in DisposablesHandler", disposablesOrdered.Count());
