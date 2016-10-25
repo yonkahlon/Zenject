2cb4364bddb7ec20357bbe8a0c39e426f92302d1
Steve Vermeulen <sfvermeulen@gmail.com>
2016-10-25 10:29:54 -0300

    Fixed typo in comment #165

--- UnityProject/Assets/Zenject/Source/Editor/Testing/ZenjectUnitTestFixture.cs
+++ UnityProject/Assets/Zenject/Source/Editor/Testing/ZenjectUnitTestFixture.cs
@@ -13,7 +13,7 @@ namespace Zenject
     // Inherit from this and mark you class with [TestFixture] attribute to do some unit tests
     // For anything more complicated than this, such as tests involving interaction between
     // several classes, or if you want to use interfaces such as IInitializable or IDisposable,
-    // then I recommend using ZenjectIntegrationUnitTestFixture instead
+    // then I recommend using ZenjectIntegrationTestFixture instead
     // See documentation for details
     public abstract class ZenjectUnitTestFixture
     {
