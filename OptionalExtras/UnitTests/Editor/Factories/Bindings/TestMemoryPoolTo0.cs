dbd29170a3fa53eae3ac77c0c6f53263047dfbd9
Steve Vermeulen <sfvermeulen@gmail.com>
2017-01-29 13:10:21 -0400

    Added more tests for memory pools

--- OptionalExtras/UnitTests/Editor/Factories/Bindings/TestMemoryPoolTo0.cs
+++ OptionalExtras/UnitTests/Editor/Factories/Bindings/TestMemoryPoolTo0.cs
@@ -7,124 +7,124 @@ using Assert=ModestTree.Assert;
 namespace Zenject.Tests.Bindings
 {
     [TestFixture]
-    public class TestPooledFactoryTo0 : ZenjectUnitTestFixture
+    public class TestMemoryPoolTo0 : ZenjectUnitTestFixture
     {
         [Test]
         public void TestFactoryProperties()
         {
-            Container.BindPooledFactory<Foo, Foo.Factory>();
+            Container.BindMemoryPool<Foo, Foo.Pool>();
 
-            var factory = Container.Resolve<Foo.Factory>();
+            var factory = Container.Resolve<Foo.Pool>();
 
             Assert.IsEqual(factory.NumActive, 0);
-            Assert.IsEqual(factory.NumCreated, 0);
+            Assert.IsEqual(factory.NumTotal, 0);
             Assert.IsEqual(factory.NumInactive, 0);
 
             var foo = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 1);
-            Assert.IsEqual(factory.NumCreated, 1);
+            Assert.IsEqual(factory.NumTotal, 1);
             Assert.IsEqual(factory.NumInactive, 0);
             Assert.IsEqual(foo.ResetCount, 1);
 
             factory.Despawn(foo);
 
             Assert.IsEqual(factory.NumActive, 0);
-            Assert.IsEqual(factory.NumCreated, 1);
+            Assert.IsEqual(factory.NumTotal, 1);
             Assert.IsEqual(factory.NumInactive, 1);
             Assert.IsEqual(foo.ResetCount, 1);
 
             foo = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 1);
-            Assert.IsEqual(factory.NumCreated, 1);
+            Assert.IsEqual(factory.NumTotal, 1);
             Assert.IsEqual(factory.NumInactive, 0);
             Assert.IsEqual(foo.ResetCount, 2);
 
             var foo2 = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 2);
-            Assert.IsEqual(factory.NumCreated, 2);
+            Assert.IsEqual(factory.NumTotal, 2);
             Assert.IsEqual(factory.NumInactive, 0);
             Assert.IsEqual(foo2.ResetCount, 1);
 
             factory.Despawn(foo);
 
             Assert.IsEqual(factory.NumActive, 1);
-            Assert.IsEqual(factory.NumCreated, 2);
+            Assert.IsEqual(factory.NumTotal, 2);
             Assert.IsEqual(factory.NumInactive, 1);
             Assert.IsEqual(foo.ResetCount, 2);
 
             factory.Despawn(foo2);
 
             Assert.IsEqual(factory.NumActive, 0);
-            Assert.IsEqual(factory.NumCreated, 2);
+            Assert.IsEqual(factory.NumTotal, 2);
             Assert.IsEqual(factory.NumInactive, 2);
         }
 
         [Test]
         public void TestExpandDouble()
         {
-            Container.BindPooledFactory<Foo, Foo.Factory>().ExpandByDoubling();
+            Container.BindMemoryPool<Foo, Foo.Pool>().ExpandByDoubling();
 
-            var factory = Container.Resolve<Foo.Factory>();
+            var factory = Container.Resolve<Foo.Pool>();
 
             Assert.IsEqual(factory.NumActive, 0);
-            Assert.IsEqual(factory.NumCreated, 0);
+            Assert.IsEqual(factory.NumTotal, 0);
             Assert.IsEqual(factory.NumInactive, 0);
 
             var foo = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 1);
-            Assert.IsEqual(factory.NumCreated, 1);
+            Assert.IsEqual(factory.NumTotal, 1);
             Assert.IsEqual(factory.NumInactive, 0);
 
             var foo2 = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 2);
-            Assert.IsEqual(factory.NumCreated, 2);
+            Assert.IsEqual(factory.NumTotal, 2);
             Assert.IsEqual(factory.NumInactive, 0);
 
             var foo3 = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 3);
-            Assert.IsEqual(factory.NumCreated, 4);
+            Assert.IsEqual(factory.NumTotal, 4);
             Assert.IsEqual(factory.NumInactive, 1);
 
             factory.Despawn(foo2);
 
             Assert.IsEqual(factory.NumActive, 2);
-            Assert.IsEqual(factory.NumCreated, 4);
+            Assert.IsEqual(factory.NumTotal, 4);
             Assert.IsEqual(factory.NumInactive, 2);
 
             var foo4 = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 3);
-            Assert.IsEqual(factory.NumCreated, 4);
+            Assert.IsEqual(factory.NumTotal, 4);
             Assert.IsEqual(factory.NumInactive, 1);
         }
 
         [Test]
         public void TestFixedSize()
         {
-            Container.BindPooledFactory<Foo, Foo.Factory>().WithFixedSize(2);
+            Container.BindMemoryPool<Foo, Foo.Pool>().WithFixedSize(2);
 
-            var factory = Container.Resolve<Foo.Factory>();
+            var factory = Container.Resolve<Foo.Pool>();
 
             Assert.IsEqual(factory.NumActive, 0);
-            Assert.IsEqual(factory.NumCreated, 2);
+            Assert.IsEqual(factory.NumTotal, 2);
             Assert.IsEqual(factory.NumInactive, 2);
 
             var foo = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 1);
-            Assert.IsEqual(factory.NumCreated, 2);
+            Assert.IsEqual(factory.NumTotal, 2);
             Assert.IsEqual(factory.NumInactive, 1);
 
             var foo2 = factory.Spawn();
 
             Assert.IsEqual(factory.NumActive, 2);
-            Assert.IsEqual(factory.NumCreated, 2);
+            Assert.IsEqual(factory.NumTotal, 2);
             Assert.IsEqual(factory.NumInactive, 0);
 
             Assert.Throws<PoolExceededFixedSizeException>(() => factory.Spawn());
@@ -133,12 +133,12 @@ namespace Zenject.Tests.Bindings
         [Test]
         public void TestInitialSize()
         {
-            Container.BindPooledFactory<Foo, Foo.Factory>().WithInitialSize(5);
+            Container.BindMemoryPool<Foo, Foo.Pool>().WithInitialSize(5);
 
-            var factory = Container.Resolve<Foo.Factory>();
+            var factory = Container.Resolve<Foo.Pool>();
 
             Assert.IsEqual(factory.NumActive, 0);
-            Assert.IsEqual(factory.NumCreated, 5);
+            Assert.IsEqual(factory.NumTotal, 5);
             Assert.IsEqual(factory.NumInactive, 5);
         }
 
@@ -148,12 +148,12 @@ namespace Zenject.Tests.Bindings
             {
             }
 
-            public class Factory : Factory<Bar>
+            public class Pool : MemoryPool<Bar>
             {
             }
         }
 
-        class Foo : IPoolable
+        class Foo
         {
             public Foo()
             {
@@ -164,26 +164,22 @@ namespace Zenject.Tests.Bindings
                 get; private set;
             }
 
-            public void OnDespawned()
+            public class Pool : MemoryPool<Foo>
             {
-            }
-
-            public void OnSpawned()
-            {
-                ResetCount++;
-            }
-
-            public class Factory : PooledFactory<Foo>
-            {
+                protected override void OnSpawned(Foo foo)
+                {
+                    foo.ResetCount++;
+                }
             }
         }
 
         [Test]
         public void TestSubContainers()
         {
-            Container.BindPooledFactory<Qux, Qux.Factory>().FromSubContainerResolve().ByMethod(InstallQux).NonLazy();
+            Container.BindMemoryPool<Qux, Qux.Pool>()
+                .FromSubContainerResolve().ByMethod(InstallQux).NonLazy();
 
-            var factory = Container.Resolve<Qux.Factory>();
+            var factory = Container.Resolve<Qux.Pool>();
             var qux = factory.Spawn();
         }
 
@@ -192,17 +188,9 @@ namespace Zenject.Tests.Bindings
             subContainer.Bind<Qux>().AsSingle();
         }
 
-        class Qux : IPoolable
+        class Qux
         {
-            public void OnDespawned()
-            {
-            }
-
-            public void OnSpawned()
-            {
-            }
-
-            public class Factory : PooledFactory<Qux>
+            public class Pool : MemoryPool<Qux>
             {
             }
         }
